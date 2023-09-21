354d01f1da661f181a454c09b4d1683f8e563d5a
Douglas <104176778+douglads@users.noreply.github.com>
2023-09-21 20:56:07 -0300

    Update Teste.java

--- app/src/main/java/calculadora/Divisao/Teste.java
+++ app/src/main/java/calculadora/Divisao/Teste.java
@@ -9,7 +9,7 @@ package calculadora.Divisao;
  * @author aluno
  */
 /*TODO divisão por 0 gera erro, divisão que o resto não é 0 por se tratar números inteiros o resto é ignorado
-Há limite nos parâmetros também para números por se tratar de inteiro*/
+Há limite nos parâmetros também para números por se tratar de inteiro, e números divididos por 0 devem ter tratamento em de try catch*/
 public class Teste {
     Divisao d = new Divisao();
     public void teste(){
